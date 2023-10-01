5a0da0b64a10be43abe1545aeae86bf9ebb96891
Shusei Nakahara <shusei.nakahara@gmail.com>
2023-10-01 15:05:51 +0900

    improve user interface

--- resources/views/ramens/create.blade.php
+++ resources/views/ramens/create.blade.php
@@ -1,4 +1,9 @@
 <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">投稿</h2>
+        <p class="mt-1 text-sm leading-6 text-gray-600">最近食べたラーメンの写真を投稿しよう</p>
+    </x-slot>
+
     @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
@@ -8,22 +13,35 @@
             </ul>
         </div>
     @endif
-    <form action="{{ route('ramens.store') }}" method="post" enctype="multipart/form-data">
-        @csrf
-        <div>
-            <label for="media">アップロードする写真を選択してください</label>
-            <input type="file" id="media" name="media" accept="image/*"/>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    <form action="{{ route('ramens.store') }}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="mt-6">
+                            <label for="media" class="block text-sm font-medium leading-6 text-gray-900">写真</label>
+                            <input type="file" id="media" name="media" accept="image/*" class="mt-2 rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"/>
+                        </div>
+                        <div class="mt-6 sm:col-span-4">
+                            <label for="restaurant" class="block text-sm font-medium leading-6 text-gray-900">店名</label>
+                            <div class="mt-2">
+                                <input type="text" name="restaurant" id="restaurant" value="{{ old('restaurant') }}" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            </div>
+                        </div>
+                        <div class="mt-6 sm:col-span-4">
+                            <label for="branch" class="block text-sm font-medium leading-6 text-gray-900">支店名 (任意)</label>
+                            <div class="mt-2">
+                                <input type="text" name="branch" id="branch" value="{{ old('branch') }}" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            </div>
+                        </div>
+                        <div class="mt-6">
+                            <x-primary-button>投稿</x-primary-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-        <div>
-            <label for="restaurant">店名</label>
-            <input type="text" name="restaurant" id="restaurant" value="{{ old('restaurant') }}">
-        </div>
-        <div>
-            <label for="branch">支店名 (任意)</label>
-            <input type="text" name="branch" id="branch" value="{{ old('branch') }}">
-        </div>
-        <div>
-            <button>投稿</button>
-        </div>
-    </form>
+    </div>
 </x-app-layout>
--- resources/views/ramens/index.blade.php
+++ resources/views/ramens/index.blade.php
@@ -1,18 +1,29 @@
 <x-app-layout>
-    <a href="{{ route('ramens.create') }}">{{ __('Post') }}</a>
-    <ul>
-        @foreach ($ramens as $ramen)
-            <li>
-                <img src="{{ $ramen->getFirstMediaUrl('media') }}" alt="{{ __('Ramen Image') }}">
-                <p>{{ $ramen->restaurant }}</p>
-            </li>
-            @if ($ramen->user()->is(auth()->user()))
-                <form action="{{ route('ramens.destroy', $ramen) }}" method="POST">
-                    @csrf
-                    @method('delete')
-                    <button>{{ __('Delete') }}</button>
-                </form>
-            @endif
-        @endforeach
-    </ul>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            最近の投稿
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <ul class="flex flex-wrap">
+                @foreach ($ramens as $ramen)
+                    <div class="flex w-1/3 flex-wrap">
+                        <li class="p-1 md:p-2">
+                            <img src="{{ $ramen->getFirstMediaUrl('media') }}" alt="{{ __('Ramen Image') }}" class="block h-full w-full rounded-lg object-cover object-center">
+                            <p>{{ $ramen->restaurant }}</p>
+                            @if ($ramen->user()->is(auth()->user()))
+                                <form action="{{ route('ramens.destroy', $ramen) }}" method="POST">
+                                    @csrf
+                                    @method('delete')
+                                    <button>{{ __('Delete') }}</button>
+                                </form>
+                            @endif
+                        </li>
+                    </div>
+                @endforeach
+            </ul>
+        </div>
+    </div>
 </x-app-layout>
